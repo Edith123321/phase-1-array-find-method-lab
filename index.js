bcd0a1805b62af2e0756377f80a61c11f4939927
edith.githinji <edithgithinji2020@gmail.com>
2024-12-07 16:33:54 +0300

    understanding the find() method

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+//  function superBowlWin(record){
+//     const win = record.find(games => game.team === "Denver Broncos football" && game.result==="W");
+//     return win ? win.year:undefined;
+//  }
+
+function superbowlWin(records) {
+
+    const winner = records.find(record => record.result === "W");   
+    return winner ? winner.year : undefined;
+ }
+ let yearWon = superbowlWin(record);
+ 
+
+console.log(yearWon)
